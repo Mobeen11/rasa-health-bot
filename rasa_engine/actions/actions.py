09b91b65d4b50768137b25c9254b9020069f72c8
Mobeen <mobeen@mail.uni-paderborn.de>
2022-04-11 17:39:23 +0200

    - create new form validations - improved stories and config - improved responses in domain

--- rasa_engine/actions/actions.py
+++ rasa_engine/actions/actions.py
@@ -11,6 +11,10 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.forms import FormValidationAction
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import logging
+logger = logging.getLogger(__name__)
+logger.debug("Starting Action server")
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -29,14 +33,76 @@ class ValidateHealthForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_health_form"
 
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer."""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
     async def validate_confirm_exercise(self,
                                         value: Text,
                                         dispatcher: CollectingDispatcher,
                                         tracker: Tracker,
                                         domain: Dict[Text, Any]) -> Dict[Text, Any]:
-
-        if value:
+        print("validate confirm_exercise: ", value)
+        if isinstance(value, bool):
             return {"confirm_exercise": True}
         else:
             return {"exercise": "None", "confirm_exercise": False}
 
+    async def validate_exercise(self,
+                                value: Text,
+                                dispatcher: CollectingDispatcher,
+                                tracker: Tracker,
+                                domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        print("validate exercise: ", value)
+        if isinstance(value, str):
+            return {"exercise": value}
+        return {"exercise": None}
+
+#   - stress
+#   - diet
+#   - goal
+    async def validate_sleep(self, value: Text,
+                                dispatcher: CollectingDispatcher,
+                                tracker: Tracker,
+                                domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        print("validate hours of sleep")
+        if self.is_int(value) and int(value) > 0:
+            return {"sleep": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_sleeping_hours")
+            # validation failed, set slot to None
+            return {"sleep": None}
+
+
+class ActionSubmitForm(Action):
+    def name(self) -> Text:
+        return "action_submit_form"
+
+    def run(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
+
+        print("confirm_exercise: ", confirm_exercise)
+        print("exercise: ", exercise)
+        print("sleep: ", sleep)
+        print("stress: ", stress)
+        print("diet: ", diet)
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+        return []
